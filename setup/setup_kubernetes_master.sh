d7128114211d4ac7e714060da79871ab97336629
Desnes Nunes <desnesn@linux.ibm.com>
2022-06-17 23:21:30 -0300

    fix failed to fetch error
    Signed-off-by: Desnes Nunes <desnesn@linux.ibm.com>

--- setup/setup_kubernetes_master.sh
+++ setup/setup_kubernetes_master.sh
@@ -13,7 +13,7 @@ sudo systemctl enable docker
 
 # Install kubernetes
 sudo apt install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt install -y kubeadm kubelet kubectl kubernetes-cni
 
